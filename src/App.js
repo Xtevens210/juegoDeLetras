3cc7db25494cc82a94a251d7d5a94e8d4b226db0
Xtevens210 <stevens210@hotmail.es>
2022-04-03 18:53:25 -0500

    Agg link youtube y avances de el relleno de las letras

--- src/App.js
+++ src/App.js
@@ -15,16 +15,40 @@ import FondoWebRosa from './Imagenes/FondoWeb-rosa.jpg';
 import LetraA from './Imagenes/carta-a.png';
 import LetraB from './Imagenes/letra-b.png';
 import LetraH from './Imagenes/letra-h.png';
+import LetraC from './Imagenes/letra-c.png';
+import LetraO from './Imagenes/letra-o.png';
+import LetraP from './Imagenes/letra-p.png';
+import LetraG from './Imagenes/letra-g.png';
+import LetraT from './Imagenes/letra-t.png';
+import LetraV from './Imagenes/letra-v.png';
+import LetraM from './Imagenes/letra-m.png';
+import LetraL from './Imagenes/letra-l.png';
 
 //Imagenes
-import Avion from './Imagenes/avion.png';
 import LasPistasDeBlue from './Imagenes/lasPistasDeBlue.png';
 import HakunaMatata from './Imagenes/HakunaMatata.png';
+import AvePañales from './Imagenes/Rugrats.png';
+import Bugs from './Imagenes/BugsBunny.png';
+import Olaf from './Imagenes/olaf.png';
+import Pikachu from './Imagenes/pikachu.png';
+import Goku from './Imagenes/goku.png';
+import BabyShark from './Imagenes/BabyShark.png';
+import VacaLola from './Imagenes/vacaLola.png';
+import MinieMouse from './Imagenes/MinieMouse.png';
+import LucasAraña from './Imagenes/LucasAraña.png';
 
 //Audios
 import BlueAudio from './audios/LasPistasDeBlue.ogg';
 import HakunaMatataAudio from './audios/HakunaMatata.mp3';
-
+import AvePañalesAudio from './audios/RugratsAudio.mp3';
+import LoneyTunes from './audios/IntroLoneyTunes.mp3';
+import OlafAudio from './audios/OlafAudio.mp3';
+import PokemonAudio from './audios/pokemonAudio.mp3';
+import DragonBallAudio from './audios/dragonBallAudio.mp3';
+import BabySharkAudio from './audios/babySharkAudio.mp3';
+import VacaLolaAudio from './audios/VacaLolaAudio.mp3';
+import MickyMouseAudio from './audios/mickyMouseAudio.mp3';
+import LucasArañaAudio from './audios/LucasArañaAudio.mp3';
 
 function App() {
   const [fondo, setFondo] = useState(FondoWeb1);
@@ -50,9 +74,17 @@ function App() {
       <BotonesColor color="Verde" funcion={cambioDeFondo} claseLabel="boton-verde-label" claseDiv="boton-verde-div"/>
       <BotonesColor color="Rojo" funcion={cambioDeFondo} claseLabel="boton-rojo-label" claseDiv="boton-rojo-div"/>
       <BotonesColor color="Rosa" funcion={cambioDeFondo} claseLabel="boton-rosa-label" claseDiv="boton-rosa-div"/>
-      <Ficha icono={LetraA} icono2={Avion} nombreIcono="Avion" name="Letra A"/>
-      <Ficha icono={LetraB} icono2={LasPistasDeBlue} nombreIcono="Blue" name="Letra B" audio={BlueAudio}/>
-      <Ficha icono={LetraH} icono2={HakunaMatata} nombreIcono="Hakuna Matata" name="Letra H" audio={HakunaMatataAudio}/>
+      <Ficha icono={LetraA} icono2={AvePañales} nombreIcono="Aventuras en pañales" name="Letra A" audio={AvePañalesAudio} link="https://www.youtube.com/watch?v=YIte86Os_k0&list=PLJC2CfG2fpdboo4aSGT_J2Lbw0ThIA7GS&index=2"/>
+      <Ficha icono={LetraB} icono2={LasPistasDeBlue} nombreIcono="Blue" name="Letra B" audio={BlueAudio} link="https://www.youtube.com/watch?v=S3wSlXgnl-c&list=PL2PbHaJs0TtrnNGzjV8JraYZ_vezmbUI2"/>
+      <Ficha icono={LetraC} icono2={Bugs} nombreIcono="Conejo/Bugs Bunny" name="Letra C" audio={LoneyTunes} link="https://www.youtube.com/watch?v=-X7JC9LChXY&list=PLvJKlTXowkBXJf3646UuEbWSVAkJTxIxq"/>
+      <Ficha icono={LetraG} icono2={Goku} nombreIcono="Goku" name="Letra G" audio={DragonBallAudio} link="https://www.youtube.com/watch?v=AvMT4Aquasc&list=PLEPPMZPjXfINH3RxAbbFyAbBsQsJZErmm"/>
+      <Ficha icono={LetraH} icono2={HakunaMatata} nombreIcono="Hakuna Matata" name="Letra H" audio={HakunaMatataAudio} link="https://www.youtube.com/watch?v=oPCRbYT6M7A&list=PLt-oe_SIwEuTirCGthW98h5Gv6lcCAR7f"/>
+      <Ficha icono={LetraL} icono2={LucasAraña} nombreIcono="Lucas La Araña" name="Letra L" audio={LucasArañaAudio} link="https://www.youtube.com/watch?v=bjxvf7vZuq4"/>
+      <Ficha icono={LetraM} icono2={MinieMouse} nombreIcono="Minie Mouse" name="Letra M" audio={MickyMouseAudio} link="https://www.youtube.com/watch?v=MveUdv8VxMk&list=PLpB5fBabXdoWnixsHxWyo6AA4lr8b34Hy"/>
+      <Ficha icono={LetraO} icono2={Olaf} nombreIcono="Olaf" name="Letra O" audio={OlafAudio} link="https://www.youtube.com/watch?v=CaK5vz6B1r0&list=PLiNVoBckLqLk5QeChEGK-4k3rYceryeUw"/>
+      <Ficha icono={LetraP} icono2={Pikachu} nombreIcono="Pikachu" name="Letra P" audio={PokemonAudio} link="https://www.youtube.com/watch?v=uDIoEbbFKAY&list=PLRhT4-rJUyveV3QGGFbghKQZ5g5Ta75FH&index=2"/>
+      <Ficha icono={LetraT} icono2={BabyShark} nombreIcono="Tiburon Bebe" name="Letra T" audio={BabySharkAudio} link="https://www.youtube.com/watch?v=irxVbTygChE&list=PLXJZReOlh8yLu_ZlzrnnhZv-PQN2pGrtD"/>
+      <Ficha icono={LetraV} icono2={VacaLola} nombreIcono="Vaca Lola" name="Letra V" audio={VacaLolaAudio} link="https://www.youtube.com/watch?v=7_lNBerhBq4&list=PLwUxYVNN22j-pRJDr_a_9L05_gcasast-"/>
     </div>
   );
 }
--- src/components/Ficha.js
+++ src/components/Ficha.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import BlueAudio from '../audios/LasPistasDeBlue.mp3';
+import React, { useState } from 'react';
+import youtube from '../Imagenes/youtube.png';
 
 
 export default function Ficha(props) {
@@ -8,6 +8,7 @@ export default function Ficha(props) {
     const [ nombre, setNombre ] = useState("");
     const [ div, setDiv ] = useState("ficha-div");
     const [ reproductor, setReproductor] = useState("");
+    const [ link, setLink] = useState("")
 
     const clickImagen = () => {
         if(clase === "ficha"){
@@ -16,21 +17,23 @@ export default function Ficha(props) {
             setNombre(props.nombreIcono);
             setDiv("ficha-div-click");
             setReproductor(<audio  className="audio" id="idAudio" src={props.audio} controls />);
+            setLink(<a href={props.link} title="Ir a YOUTUBE" target="_blank"><img className="link" src={youtube} alt={props.nombreIcono}/></a>);
         }else{
             setImagen(props.icono);
             setClase("ficha");
             setNombre("");
             setDiv("ficha-div");
             setReproductor("");
+            setLink("");
         }
     }
 
 
     return (
         <div className={div}>
-            <img onClick={() => clickImagen()} className={clase} src={imagen} alt={props.name}/>
-                <div>{reproductor}</div>
             <h2>{nombre}</h2>
+            <img onClick={() => clickImagen()} className={clase} src={imagen} alt={props.name}/>
+                <div>{reproductor}{link}</div>
         </div>
     )
 }
